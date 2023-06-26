daaddca2cbcd3f97e828310608f6658801d77059
caiorr1 <caiorrodrigues2004@gmail.com>
2023-06-26 15:08:03 -0300

    uptade

--- aluramidi-curso-arquivos-iniciais/main.js
+++ aluramidi-curso-arquivos-iniciais/main.js
@@ -1,2 +1,5 @@
-document.querySelector('#som_tecla_pom');
-document.querySelector('#som_tecla_pom').play();
+function tocaSomPom () {
+    document.querySelector('#som_tecla_pom').play();
+}
+document.querySelector('.tecla_pom').onclick = tocaSomPom;
+
